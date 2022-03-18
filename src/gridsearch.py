5e3442abb9bb1ceca4ac86d4b05a761016f9ce6c
Antonello Dettori <dev@dettori.io>
2022-03-18 15:38:03 +0100

    gridsearch: update list of parameters available and added some constraints

--- src/gridsearch.py
+++ src/gridsearch.py
@@ -7,13 +7,21 @@ dataset_list = ["syn5"]
 lr_list = [0.01, 0.1, 0.5, 1]
 epoch_list = [300, 500]
 beta_list = [0.1, 0.5]
-momentum_list = [0, 0.5, 0.7, 0.9]
+momentum_list = [0, 0.5, 0.9]
+edge_del_list = [True, False]
+edge_add_list = [True, False]
+bernoulli_list = [True, False]
+delta_list = [True]
 
 hyperpar_combo = {"dataset": dataset_list,
                   "lr": lr_list,
                   "num_epochs": epoch_list,
                   "beta": beta_list,
-                  "n_momentum": momentum_list}
+                  "n_momentum": momentum_list,
+                  "edge_del": edge_del_list,
+                  "edge_add": edge_add_list,
+                  "bernoulli": bernoulli_list,
+                  "delta": delta_list}
 
 dict_keys, dict_vals = zip(*hyperpar_combo.items())
 combo_list = list(it.product(*dict_vals))
@@ -26,9 +34,18 @@ start_time = time.time()
 for i, combo in enumerate(combo_list):
 
     combo_dict = {dict_keys[i]: combo[i] for i in range(len(dict_keys))}
+
+    # Need to specify at least one op
+    if not combo_dict["edge_add"] and not combo_dict["edge_del"]:
+        continue
+
+    # edge_add in the orig formulation is identical to edge_add + edge_del
+    if combo_dict["edge_add"] and combo_dict["edge_del"] and not combo_dict["delta"]:
+        continue
+
     task_list.append(delayed(main_explain)(**combo_dict))
 
-Parallel(n_jobs=-2)(task_list)
+Parallel(n_jobs=-1)(task_list)
 
 end_time = time.time()
 time_mins = (end_time-start_time)//60
