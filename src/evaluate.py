f20626c3ac296542374d85e2866a6cafda2bc24b
Antonello Dettori <dev@dettori.io>
2022-03-31 22:51:18 +0200

    evaluate: refactored code, added a metric to quantify accuracy for the addition of edges and an edit distance inspired metric for perturbations

--- src/evaluate.py
+++ src/evaluate.py
@@ -22,144 +22,182 @@ hidden = 20
 dropout = 0.0
 
 
-def evaluate(path, dataset_dict):
+def compute_accuracy_measures(cf_df, adj, dataset, dataset_id, task):
 
-    if ".ipynb" in path or ".txt" in path:
-        return None
-
-    if "syn4" in path:
-        dataset = "syn4"
-        dataset_name = "Tree-Cycles (syn4)"
-    elif "syn5" in path:
-        dataset = "syn5"
-        dataset_name = "Tree-Grid (syn5)"
-    elif "syn1" in path:
-        dataset = "syn1"
-        dataset_name = "BA-shapes (syn1)"
-
-    data = dataset_dict[dataset]
-
-    adj = torch.Tensor(data["adj"]).squeeze()  # Does not include self loops
-    features = torch.Tensor(data["feat"]).squeeze()
-    labels = torch.tensor(data["labels"]).squeeze()
-    idx_train = torch.tensor(data["train_idx"])
-    idx_test = torch.tensor(data["test_idx"])
+    # Load relevant data from dataset for model
+    features = torch.Tensor(dataset["feat"]).squeeze()
+    labels = torch.tensor(dataset["labels"]).squeeze()
+    idx_train = torch.tensor(dataset["train_idx"])
+    idx_test = torch.tensor(dataset["test_idx"])
     edge_index = dense_to_sparse(adj)
 
     # Re-assemble model
     norm_adj = normalize_adj(adj)
     model = GCNSynthetic(nfeat=features.shape[1], nhid=hidden, nout=hidden,
                          nclass=len(labels.unique()), dropout=dropout)
-    model.load_state_dict(torch.load("../models/gcn_3layer_{}.pt".format(dataset)))
-    model.eval()  # Testing mode
+    model.load_state_dict(torch.load("../models/gcn_3layer_{}.pt".format(dataset_id)))
+    model.eval()  # Model testing mode
     output = model(features, norm_adj)
     y_pred_orig = torch.argmax(output, dim=1)
 
-    num_tot_examples = None
-    # Load CF examples
-    with open(path, "rb") as f:
-        cf_examples = pickle.load(f)
-        num_tot_examples = len(cf_examples)
-        df_prep = []
-
-        for example in cf_examples:
-            # Ignore examples for which generating a CF wasn't possible
-            if example[0] != []:
-                df_prep.append(example[0])
-        df = pd.DataFrame(df_prep, columns=header)
-
-    # Add num edges for each generated CF
-    num_edges = []
-    for i in df.index:
-        # Note: diagonal is 0
-        num_edges.append(sum(sum(df["sub_adj"][i])) / 2)
-
-    df["num_edges"] = num_edges
-
-    # For accuracy, only look at motif nodes among CFs
-    df_motif = df[df["y_pred_orig"] != 0].reset_index(drop=True)
-    accuracy = []
+    # For accuracy, only look at nodes that the model believes belong to the motif
+    # Note: 0 is used to mean that the node isn't in the motif
+    df_motif = cf_df[cf_df["y_pred_orig"] != 0].reset_index(drop=True)
 
     # Get original predictions
     dict_ypred_orig = dict(zip(sorted(np.concatenate((idx_train.numpy(), idx_test.numpy()))),
                                y_pred_orig.numpy()))
 
+    accuracy = []
+
     # Compute accuracy
     for i in range(len(df_motif)):
+        # These idxs are taken from the explainer output
         node_idx = df_motif["node_idx"][i]
         new_idx = df_motif["new_idx"][i]
         _, _, _, node_dict = get_neighbourhood(int(node_idx), edge_index, 4, features, labels)
+        node_dict_inv = {value: key for key, value in node_dict.items()}
 
         # Confirm idx mapping is correct
-        if node_dict[node_idx] == df_motif["new_idx"][i]:
-
-            cf_adj = df_motif["cf_adj"][i]
-            sub_adj = df_motif["sub_adj"][i]
-            perturb = np.abs(cf_adj - sub_adj)
-
-            # Note: the accuracy is measured only wrt to deleted edges
-            pert_del_edges = sub_adj - cf_adj
-            pert_del_edges[pert_del_edges == -1] = 0
-
-            pert_add_edges = cf_adj - sub_adj
-            pert_add_edges[pert_add_edges == -1] = 0
-
-            perturb_edges = np.nonzero(pert_del_edges)  # Changed edge indices
-
-            nodes_involved = np.unique(np.concatenate((perturb_edges[0], perturb_edges[1]),
-                                                      axis=0))
-            perturb_nodes = nodes_involved[nodes_involved != new_idx]  # Remove original node
-
-            # Retrieve original node idxs for original predictions
-            perturb_nodes_orig_idx = []
-            for j in perturb_nodes:
-                perturb_nodes_orig_idx.append([key for (key, value) in node_dict.items()
-                                               if value == j])
-            perturb_nodes_orig_idx = np.array(perturb_nodes_orig_idx).flatten()
-
-            # Retrieve original predictions
-            perturb_nodes_orig_ypred = np.array([dict_ypred_orig[k]
-                                                 for k in perturb_nodes_orig_idx])
-            nodes_in_motif = perturb_nodes_orig_ypred[perturb_nodes_orig_ypred != 0]
-
-            # Handle situation in which edges are only added and accuracy is NaN
-            if perturb_edges[0].size == 0:
-                # Sanity check in case of no change to sub_adj
-                if np.nonzero(pert_add_edges)[0].size == 0 and "PP" not in path:
-                    raise RuntimeError("evaluate: sub_adj and cf_adj are identical")
-
-                prop_correct = np.NaN
-
-            else:
-                prop_correct = len(nodes_in_motif) / len(perturb_nodes_orig_idx)
-
-            accuracy.append([node_idx, new_idx, perturb_nodes_orig_idx,
-                             perturb_nodes_orig_ypred, nodes_in_motif, prop_correct])
-        else:
+        if node_dict[node_idx] != df_motif["new_idx"][i]:
             raise RuntimeError("Error in node mapping")
 
+        orig_adj = df_motif["sub_adj"][i]
+        cf_adj = df_motif["cf_adj"][i]
+
+        # Note: the accuracy is measured only wrt to deleted edges
+        pert_del_edges = orig_adj - cf_adj
+        pert_del_edges[pert_del_edges == -1] = 0
+
+        pert_add_edges = cf_adj - orig_adj
+        pert_add_edges[pert_add_edges == -1] = 0
+
+        # Changed edge indices
+        idx_del_edges = np.nonzero(pert_del_edges)
+        idx_add_edges = np.nonzero(pert_add_edges)
+
+        # Find idx of nodes involved in the edge perturbations
+        nodes_del_edges = np.unique(np.concatenate((idx_del_edges[0], idx_del_edges[1]), axis=0))
+        nodes_add_edges = np.unique(np.concatenate((idx_add_edges[0], idx_add_edges[1]), axis=0))
+
+        # Remove original node since we already know its prediction
+        nodes_del_edges = nodes_del_edges[nodes_del_edges != new_idx]
+        nodes_add_edges = nodes_add_edges[nodes_add_edges != new_idx]
+
+        # Retrieve original node idxs for original predictions
+        del_nodes_orig_idx = [node_dict_inv[new_idx] for new_idx in nodes_del_edges]
+        add_nodes_orig_idx = [node_dict_inv[new_idx] for new_idx in nodes_add_edges]
+
+        # Retrieve original predictions
+        del_nodes_orig_ypred = np.array([dict_ypred_orig[k] for k in del_nodes_orig_idx])
+        add_nodes_orig_ypred = np.array([dict_ypred_orig[k] for k in add_nodes_orig_idx])
+
+        # Retrieve nodes in motif (ground truth)
+        del_nodes_in_motif = del_nodes_orig_ypred[del_nodes_orig_ypred != 0]
+        add_nodes_in_motif = add_nodes_orig_ypred[add_nodes_orig_ypred != 0]
+
+        # Sanity check in case of no change to sub_adj
+        # In case of PP it could be that the minimal explanation is the starting graph
+        if len(del_nodes_orig_idx) == 0 and len(add_nodes_orig_idx) == 0 and task != "PP":
+            raise RuntimeError("evaluate: sub_adj and cf_adj are identical")
+
+        # Handle situation in which edges are only added and del accuracy is NaN
+        if len(del_nodes_orig_idx) == 0:
+            del_prop_correct = np.NaN
+
+        else:
+            del_prop_correct = len(del_nodes_in_motif) / len(del_nodes_orig_idx)
+
+        if len(add_nodes_orig_idx) == 0:
+            add_prop_correct = np.NaN
+
+        else:
+            add_prop_correct = len(add_nodes_in_motif) / len(add_nodes_orig_idx)
+
+        accuracy.append([node_idx, new_idx, del_prop_correct, add_prop_correct])
+
     df_accuracy = pd.DataFrame(accuracy, columns=["node_idx", "new_idx",
-                                                  "perturb_nodes_orig_idx",
-                                                  "perturb_nodes_orig_ypred",
-                                                  "nodes_in_motif", "prop_correct"])
-    num_cf_examples = len(df)
+                                                  "del_prop_correct", "add_prop_correct"])
+    return df_accuracy
 
-    if "PP" in path:
-        fidelity = num_cf_examples / num_tot_examples
+
+def compute_edit_distance(expl_df):
+
+    edit_dist_list = []
+
+    for idx, row in expl_df.iterrows():
+
+        orig_adj = row["sub_adj"]
+        expl_adj = row["cf_adj"]
+
+        # Compute edges affected by del and add operations respectively
+        pert_del_edges = orig_adj - expl_adj
+        pert_del_edges[pert_del_edges == -1] = 0
+
+        pert_add_edges = expl_adj - orig_adj
+        pert_add_edges[pert_add_edges == -1] = 0
+
+        del_edits = np.sum(pert_del_edges) / 2
+        add_edits = np.sum(pert_add_edges) / 2
+
+        edit_dist = add_edits - del_edits
+        edit_dist_list.append(edit_dist)
+
+    return np.average(edit_dist_list)
+
+
+def evaluate(expl_list, dataset_id, dataset_name, dataset_data, task, accuracy_bool=True):
+
+    # Note: no self-connections in syn*
+    adj = torch.Tensor(dataset_data["adj"]).squeeze()
+
+    # Explanation count
+    num_tot_expl = None
+    num_valid_expl = None
+
+    # Build CF examples dataframe
+    num_tot_expl = len(expl_list)
+    df_prep = []
+
+    for expl in expl_list:
+        # Ignore elements for which generating an explanation wasn't possible
+        if expl[0] == []:
+            continue
+
+        df_prep.append(expl[0])
+
+    num_valid_expl = len(df_prep)
+    expl_df = pd.DataFrame(df_prep, columns=header)
+
+    # Add num edges for each generated explanation
+    expl_df["num_edges"] = expl_df["sub_adj"].transform(lambda x: np.sum(x)/2)
+    avg_edit_dist = compute_edit_distance(expl_df)
+
+    if accuracy_bool:
+        # Compute different accuracy metrics
+        accuracy_df = compute_accuracy_measures(expl_df, adj, dataset_data, dataset_id, task)
+
+    if task == "PP":
+        fidelity = num_valid_expl / num_tot_expl
     else:
-        fidelity = 1 - num_cf_examples / num_tot_examples
-    avg_graph_dist = np.mean(df["loss_graph_dist"])
-    avg_sparsity = np.mean(1 - df["loss_graph_dist"] / df["num_edges"])
-    avg_accuracy = np.mean(df_accuracy["prop_correct"])
+        # PN and counterfactual case
+        fidelity = 1 - num_valid_expl / num_tot_expl
+    avg_graph_dist = np.mean(expl_df["loss_graph_dist"])
+    avg_sparsity = np.mean(1 - expl_df["loss_graph_dist"] / expl_df["num_edges"])
 
-    results = {"path": path,
-               "dataset": dataset_name,
-               "num_cf": num_cf_examples,
-               "num_examples": num_tot_examples,
+    results = {"dataset": dataset_name,
+               "num_valid_expl": num_valid_expl,
+               "num_examples": num_tot_expl,
                "fidelity": fidelity,
                "avg_graph_dist": avg_graph_dist,
-               "avg_sparsity": avg_sparsity,
-               "avg_accuracy": avg_accuracy}
+               "avg_edit_dist": avg_edit_dist,
+               "avg_sparsity": avg_sparsity}
+
+    if accuracy_bool:
+        avg_del_accuracy = np.mean(accuracy_df["del_prop_correct"])
+        avg_add_accuracy = np.mean(accuracy_df["add_prop_correct"])
+        results["avg_del_accuracy"] = avg_del_accuracy
+        results["avg_add_accuracy"] = avg_add_accuracy
 
     return results
 
@@ -179,10 +217,33 @@ def evaluate_path_content(res_path):
         for file in files:
             path = os.path.join(subdir, file)
 
-            res = evaluate(path, dataset_dict)
-
-            if res is None:
+            # Skip irrelevant path
+            if ".ipynb" in path or ".txt" in path or ".csv" in path:
                 continue
+
+            # Extract some info from path
+            if "syn4" in path:
+                dataset_id = "syn4"
+                dataset_name = "Tree-Cycles (syn4)"
+            elif "syn5" in path:
+                dataset_id = "syn5"
+                dataset_name = "Tree-Grid (syn5)"
+            elif "syn1" in path:
+                dataset_id = "syn1"
+                dataset_name = "BA-shapes (syn1)"
+
+            if "PP" in path:
+                task = "PP"
+            elif "PN" in path:
+                task = "PN"
+            else:
+                task = "CF"
+
+            with open(path, "rb") as f:
+                generated_expl = pickle.load(f)
+
+            res = evaluate(generated_expl, dataset_id, dataset_name, dataset_dict[dataset_id], task)
+            res["path"] = path
 
             result_list.append(res)
 
