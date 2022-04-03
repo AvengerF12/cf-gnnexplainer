13aeac0c261e2da09c01311d95199d2bc2a648ce
Antonello Dettori <dev@dettori.io>
2022-04-03 15:28:04 +0200

    train_model: add support for training graph classification model

--- src/train_model.py
+++ src/train_model.py
@@ -12,11 +12,93 @@ from datasets import SyntheticDataset, MUTAGDataset
 from gcn import GCNSynthetic
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
+
 # Adapted from GNNExplainer paper in order to have similar results to CF-GNNExplainer
+def train_graph_classifier(G_dataset, model, args):
+    train_idx, test_idx = G_dataset.split_tr_ts_idx(train_ratio=args.train_ratio)
+
+    train_labels = dataset.labels[train_idx]
+    test_labels = dataset.labels[test_idx]
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    model.train()
+    ypred = None
+
+    for epoch in range(args.num_epochs):
+        begin_time = time.time()
+
+        train_ypred = []
+
+        # TODO: add mini-batch learning support
+        for idx in train_idx:
+            model.zero_grad()
+
+            adj, feat, label = dataset[idx]
+
+            norm_adj = normalize_adj(adj)
+
+            if args.cuda:
+                ypred = model(feat.cuda(), norm_adj.cuda())
+            else:
+                ypred = model(feat, norm_adj)
+
+            if args.cuda:
+                loss = model.loss(ypred, label.cuda())
+            else:
+                loss = model.loss(ypred, label)
+
+            loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+            optimizer.step()
+
+            ypred_label = torch.argmax(ypred, axis=0)
+            train_ypred.append(ypred_label)
+
+        elapsed = time.time() - begin_time
+
+        if epoch % 10 == 0:
+            print(
+                "epoch: ",
+                epoch,
+                "; loss: ",
+                loss.item(),
+                "; train_acc: ",
+                accuracy_score(train_ypred, train_labels),
+                "; train_prec: ",
+                precision_score(train_ypred, train_labels, average=None),
+                "{0:0.2f}".format(elapsed),
+            )
+
+    test_ypred = []
+
+    for idx in test_idx:
+        adj, feat, label = dataset[idx]
+
+        norm_adj = normalize_adj(adj)
+
+        if args.cuda:
+            ypred = model(feat.cuda(), norm_adj.cuda())
+        else:
+            ypred = model(feat, norm_adj)
+
+        ypred_label = torch.argmax(ypred, axis=0)
+        test_ypred.append(ypred_label)
+
+    print(
+        "test_acc: ",
+        accuracy_score(test_ypred, test_labels),
+        "; test_prec: ",
+        precision_score(test_ypred, test_labels, average=None),
+    )
+
+    torch.save(model.state_dict(), "../models/gcn_3layer_{}.pt".format(args.dataset))
+
+
 def train_node_classifier(G_dataset, model, args):
 
     train_idx, test_idx = G_dataset.split_tr_ts_idx(train_ratio=args.train_ratio)
 
+    # Train on whole adj since its a single connected component (valid for sun*, results may vary)
     adj = G_dataset.adj
     feat = G_dataset.features
     labels_train = G_dataset.labels[train_idx]
@@ -95,3 +177,10 @@ if __name__ == "__main__":
                              args.dropout)
 
         train_node_classifier(dataset, model, args)
+
+    elif args.dataset == "MUTAG":
+        dataset = MUTAGDataset(args.dataset)
+        model = GCNSynthetic(dataset.n_features, args.hidden, args.hidden, dataset.n_classes,
+                             args.dropout, True, dataset.max_num_nodes)
+
+        train_graph_classifier(dataset, model, args)
