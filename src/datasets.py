b44b16e317d8d7e5fa8826b97b8b842a9c7d03e9
Antonello Dettori <dev@dettori.io>
2022-04-03 15:26:58 +0200

    datasets: tweak MUTAG pre-processing

--- src/datasets.py
+++ src/datasets.py
@@ -57,58 +57,80 @@ class SyntheticDataset(Dataset):
 
 class MUTAGDataset(Dataset):
 
-    def __init__(self, dataset_id, train_bool=True, train_ratio=0.1):
+    def __init__(self, dataset_id):
 
         # TODO: Add error if folder not present
         path = "../data/MUTAG/"
 
         # Sparse adjacency matrix
         sparse_np_adj = np.loadtxt(path + "/MUTAG_A.txt", delimiter=", ", dtype=int, unpack=True)
+
         # Features: use node labels as features for graph classification
         np_features = np.loadtxt(path + "/MUTAG_node_labels.txt", dtype=int, unpack=True)
-        self.features = torch.LongTensor(np_features)
+        tensor_features = torch.LongTensor(np_features)
+        self.features = torch.nn.functional.one_hot(tensor_features)
+
         # Graph indicator: tells which node belongs to which graph
         np_array_g_ind = np.loadtxt(path + "/MUTAG_graph_indicator.txt", dtype=int, unpack=True)
+
         # Graph labels
         np_labels = np.loadtxt(path + "/MUTAG_graph_labels.txt", dtype=int, unpack=True)
         self.labels = torch.LongTensor(np_labels)
+        # Re-label -1 label to 0 to avoid problems with loss
+        self.labels[self.labels == -1] = 0
 
         # adj contains all the single disconnected graphs, need to split them apart
         self.adj = to_dense_adj(torch.LongTensor(sparse_np_adj)).squeeze()
+        # Need to remove the top row and the left most col since the nodes in dataset start from 1
+        self.adj = self.adj[1:,:][:, 1:]
 
         # Group nodes by graph indicator
         graphs_df = pd.DataFrame(np_array_g_ind, columns=["Indicator"])
         self.nodes_by_graph_dict = graphs_df.groupby("Indicator").indices
 
-        self.adj_by_graph_dict = {}
-        self.feat_by_graph_dict = {}
+        self.adj_by_graph_arr = []
+        self.feat_by_graph_arr = []
+
+        # Keep track of biggest graph for padding purposes
+        self.max_num_nodes = 0
 
         for graph_ind, node_array in self.nodes_by_graph_dict.items():
-            self.adj_by_graph_dict[graph_ind] = self.adj[node_array, :][:, node_array]
-            self.feat_by_graph_dict[graph_ind] = self.features[node_array]
+            num_nodes = len(node_array)
+
+            if num_nodes > self.max_num_nodes:
+                self.max_num_nodes = num_nodes
+
+            self.adj_by_graph_arr.append(self.adj[node_array, :][:, node_array])
+            self.feat_by_graph_arr.append(self.features[node_array])
 
         # The node features are their label
-        self.n_features = 1
+        self.n_features = self.features.shape[1]
         self.n_classes = len(np.unique(self.labels))
         self.task = "graph-class"
 
     def __len__(self):
-        return len(self.adj_by_graph_dict)
+        return len(self.adj_by_graph_arr)
 
     def __getitem__(self, idx):
 
-        # The graph idx start from 1
-        sub_adj = self.adj_by_graph_dict[idx+1]
-        sub_feat = self.feat_by_graph_dict[idx+1]
-        sub_labels = self.labels[idx+1]
+        cur_adj = self.adj_by_graph_arr[idx]
+        num_nodes = cur_adj.shape[0]
+        adj_padded = torch.zeros((self.max_num_nodes, self.max_num_nodes))
+        adj_padded[:num_nodes, :num_nodes] = cur_adj
 
-        return sub_adj, sub_feat, sub_labels
+        cur_feat = self.feat_by_graph_arr[idx]
+        feat_padded = torch.zeros((self.max_num_nodes, self.n_features))
+        feat_padded[:num_nodes, :self.n_features] = cur_feat
+
+        label = self.labels[idx]
+
+        return adj_padded, feat_padded, label
 
     def split_tr_ts_idx(self, train_ratio=0.8):
 
         num_graphs = len(self)
         num_train = int(num_graphs * train_ratio)
-        idx = range(num_graphs)
+        idx = list(range(num_graphs))
 
         np.random.shuffle(idx)
         train_idx = idx[:num_train]
