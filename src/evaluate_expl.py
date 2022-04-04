6b8c60c9b9826ff2c0ce3d7d10350e73046329d7
Antonello Dettori <dev@dettori.io>
2022-04-04 22:37:07 +0200

    evaluate: add initial MUTAG support

--- src/evaluate_expl.py
+++ src/evaluate_expl.py
@@ -12,6 +12,7 @@ import torch
 from torch_geometric.utils import dense_to_sparse
 from gcn import GCNSynthetic
 from utils.utils import normalize_adj, get_neighbourhood
+import datasets
 
 
 default_path = "../results/"
@@ -22,9 +23,11 @@ hidden = 20
 dropout = 0.0
 
 
-def compute_accuracy_measures(cf_df, adj, dataset, dataset_id, task):
+def compute_accuracy_measures(cf_df, dataset, dataset_id, task):
 
     # Load relevant data from dataset for model
+    # Note: no self-connections in syn*
+    adj = torch.Tensor(dataset["adj"]).squeeze()
     features = torch.Tensor(dataset["feat"]).squeeze()
     labels = torch.tensor(dataset["labels"]).squeeze()
     idx_train = torch.tensor(dataset["train_idx"])
@@ -32,11 +35,12 @@ def compute_accuracy_measures(cf_df, adj, dataset, dataset_id, task):
     edge_index = dense_to_sparse(adj)
 
     # Re-assemble model
-    norm_adj = normalize_adj(adj)
     model = GCNSynthetic(nfeat=features.shape[1], nhid=hidden, nout=hidden,
-                         nclass=len(labels.unique()), dropout=dropout)
+                         nclass=len(labels.unique()), dropout=dropout, task="node-class")
     model.load_state_dict(torch.load("../models/gcn_3layer_{}.pt".format(dataset_id)))
     model.eval()  # Model testing mode
+
+    norm_adj = normalize_adj(adj)
     output = model(features, norm_adj)
     y_pred_orig = torch.argmax(output, dim=1)
 
@@ -148,10 +152,7 @@ def compute_edit_distance(expl_df):
     return np.average(edit_dist_list)
 
 
-def evaluate(expl_list, dataset_id, dataset_name, dataset_data, task, accuracy_bool=True):
-
-    # Note: no self-connections in syn*
-    adj = torch.Tensor(dataset_data["adj"]).squeeze()
+def evaluate(expl_list, dataset_id, dataset_name, dataset_data, expl_task, accuracy_bool=True):
 
     # Explanation count
     num_tot_expl = None
@@ -175,15 +176,16 @@ def evaluate(expl_list, dataset_id, dataset_name, dataset_data, task, accuracy_b
     expl_df["num_edges"] = expl_df["sub_adj"].transform(lambda x: np.sum(x)/2)
     avg_edit_dist = compute_edit_distance(expl_df)
 
-    if accuracy_bool:
+    if accuracy_bool and "syn" in dataset_id:
         # Compute different accuracy metrics
-        accuracy_df = compute_accuracy_measures(expl_df, adj, dataset_data, dataset_id, task)
+        accuracy_df = compute_accuracy_measures(expl_df, dataset_data, dataset_id, expl_task)
 
-    if task == "PP":
+    if expl_task == "PP":
         fidelity = num_valid_expl / num_tot_expl
     else:
         # PN and counterfactual case
         fidelity = 1 - num_valid_expl / num_tot_expl
+
     avg_graph_dist = np.mean(expl_df["loss_graph_dist"])
     avg_sparsity = np.mean(1 - expl_df["loss_graph_dist"] / expl_df["num_edges"])
 
@@ -195,11 +197,14 @@ def evaluate(expl_list, dataset_id, dataset_name, dataset_data, task, accuracy_b
                "avg_edit_dist": avg_edit_dist,
                "avg_sparsity": avg_sparsity}
 
-    if accuracy_bool:
+    if accuracy_bool and "syn" in dataset_id:
         avg_del_accuracy = np.mean(accuracy_df["del_prop_correct"])
         avg_add_accuracy = np.mean(accuracy_df["add_prop_correct"])
         results["avg_del_accuracy"] = avg_del_accuracy
         results["avg_add_accuracy"] = avg_add_accuracy
+    else:
+        results["avg_del_accuracy"] = np.NaN
+        results["avg_add_accuracy"] = np.NaN
 
     return results
 
@@ -207,13 +212,14 @@ def evaluate(expl_list, dataset_id, dataset_name, dataset_data, task, accuracy_b
 def evaluate_path_content(res_path):
 
     result_list = []
+    # MUTAG evaluation doesn't need the dataset since it cannot compute accuracy
     dataset_list = ["syn1", "syn4", "syn5"]
-    dataset_dict = {}
+    dataset_dict = {"MUTAG": None}
 
     for dataset in dataset_list:
+
         with open("../data/gnn_explainer/{}.pickle".format(dataset), "rb") as f:
             dataset_dict[dataset] = pickle.load(f)
-
 
     for subdir, dirs, files in os.walk(res_path):
         for file in files:
@@ -233,18 +239,22 @@ def evaluate_path_content(res_path):
             elif "syn1" in path:
                 dataset_id = "syn1"
                 dataset_name = "BA-shapes (syn1)"
+            elif "MUTAG" in path:
+                dataset_id = "MUTAG"
+                dataset_name = "Mutagenicity"
 
             if "PP" in path:
-                task = "PP"
+                expl_task = "PP"
             elif "PN" in path:
-                task = "PN"
+                expl_task = "PN"
             else:
-                task = "CF"
+                expl_task = "CF"
 
             with open(path, "rb") as f:
                 generated_expl = pickle.load(f)
 
-            res = evaluate(generated_expl, dataset_id, dataset_name, dataset_dict[dataset_id], task)
+            res = evaluate(generated_expl, dataset_id, dataset_name, dataset_dict[dataset_id],
+                           expl_task)
             res["path"] = path
 
             result_list.append(res)
