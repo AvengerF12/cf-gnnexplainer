ddc507a3e0527c5093a86090e21e5b8cb47c142f
Antonello Dettori <dev@dettori.io>
2022-04-25 02:00:19 +0200

    evaluate_model: add support for batched data in node classification tasks

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -5,6 +5,7 @@ import pickle
 import torch
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 sys.path.append('../../')
+from torch.utils.data import SubsetRandomSampler, DataLoader
 from models import GCNSynthetic, GraphAttNet
 import datasets
 
@@ -28,26 +29,29 @@ def evaluate_model(dataset, dataset_id):
 
     train_idx_list, test_idx_list = dataset.split_tr_ts_idx()
 
+    ts_idx_sampler = SubsetRandomSampler(test_idx_list)
+    ts_dataloader = DataLoader(dataset, sampler=ts_idx_sampler)
+
     y_pred_list = []
     y_label_list = []
 
-    for idx in test_idx_list:
+    for idx, data in enumerate(ts_dataloader):
 
         if dataset.task == "node-class":
-            sub_adj, sub_feat, sub_labels, orig_idx, new_idx, num_nodes = dataset[idx]
+            sub_adj, sub_feat, sub_labels, orig_idx, new_idx, num_nodes = data
 
         elif dataset.task == "graph-class":
-            sub_adj, sub_feat, sub_labels, num_nodes = dataset[idx]
+            sub_adj, sub_feat, sub_labels, num_nodes = data
 
         output = model(sub_feat, sub_adj)
 
         if dataset.task == "node-class":
-            y_pred = torch.argmax(output, dim=1)
-            y_pred = y_pred[new_idx]
-            sub_labels = sub_labels[new_idx]
+            y_pred = torch.argmax(output, dim=2)
+            y_pred = y_pred[:, new_idx]
+            sub_labels = sub_labels[:, new_idx]
 
         elif dataset.task == "graph-class":
-            y_pred = torch.argmax(output, dim=0)
+            y_pred = torch.argmax(output, dim=1)
 
         y_pred_list.append(y_pred)
         y_label_list.append(sub_labels)
@@ -60,7 +64,7 @@ def evaluate_model(dataset, dataset_id):
 
 
 if __name__ == "__main__":
-    dataset_list = ["syn1", "syn4", "syn5", "MUTAG"]
+    dataset_list = ["MUTAG", "syn1", "syn4", "syn5"]
     dataset_dict = {}
 
     for dataset in dataset_list:
