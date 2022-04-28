32d23df97b2666d485fa9e143416ccfca651a32c
Antonello Dettori <dev@dettori.io>
2022-04-28 14:55:13 +0200

    visualize: update to work with new explanation format

--- src/visualize.py
+++ src/visualize.py
@@ -8,16 +8,19 @@ import pandas as pd
 import torch
 
 
-header = ["node_idx", "new_idx", "cf_adj", "sub_adj", "sub_feat", "y_pred_orig",
-          "y_pred_new_actual", "label", "num_nodes", "loss_graph_dist"]
+header_data = ["node_idx", "new_idx", "expl_list", "sub_adj", "sub_feat", "label", "y_pred_orig",
+               "num_nodes"]
+# Structure of single element in expl_list
+header_expl = ["cf_adj", "y_pred_new_actual", "loss_graph_dist"]
 
-def visualize_mutag(df, idx_cf, figsize=(20,15)):
+def visualize_mutag(df, idx_ex, idx_hist, figsize=(20,15)):
 
     mol_dict = {0: "C", 1: "N", 2: "O", 3: "F", 4: "I", 5: "Cl", 6: "Br"}
-    node_labels = torch.argmax(df["sub_feat"][idx_cf], dim=1).numpy()
+    node_labels = torch.argmax(df["sub_feat"][idx_ex], dim=1).numpy()
+    cur_expl = df["expl_list"][idx_ex][idx_hist]
 
-    sub_adj = torch.Tensor(df["sub_adj"][idx_cf])
-    cf_adj = torch.Tensor(df["cf_adj"][idx_cf])
+    sub_adj = torch.Tensor(df["sub_adj"][idx_ex])
+    cf_adj = torch.Tensor(cur_expl[0])
 
     deleted_edges = sub_adj.int() - cf_adj.int()
     deleted_edges[deleted_edges == -1] = 0
@@ -25,11 +28,11 @@ def visualize_mutag(df, idx_cf, figsize=(20,15)):
     added_edges = cf_adj.int() - sub_adj.int()
     added_edges[added_edges == -1] = 0
 
-    print("Graph label: {}".format(df["label"][idx_cf]))
-    print("Graph distance loss: {}".format(df["loss_graph_dist"][idx_cf]))
+    print("Graph label: {}".format(df["label"][idx_ex]))
+    print("Graph distance loss: {}".format(cur_expl[2]))
     print("Original prediction: {}, new prediction: {}"
-          .format(df["y_pred_orig"][idx_cf], df["y_pred_new_actual"][idx_cf]))
-    num_nodes = df["num_nodes"][idx_cf]
+          .format(df["y_pred_orig"][idx_ex], cur_expl[1]))
+    num_nodes = df["num_nodes"][idx_ex]
     print("Num of nodes: {}".format(num_nodes))
     num_edges = len(torch.nonzero(sub_adj)) / 2
     print("Num of total edges: {}".format(num_edges))
@@ -57,10 +60,12 @@ def visualize_mutag(df, idx_cf, figsize=(20,15)):
     nx.draw_networkx_labels(adj_graph, pos, {k: mol_dict[node_labels[k]] for k in range(num_nodes)})
 
 
-def visualize_generic(df, idx_cf, figsize=(20,15)):
+def visualize_generic(df, idx_ex, idx_hist, figsize=(20,15)):
 
-    sub_adj = torch.Tensor(df["sub_adj"][idx_cf])
-    cf_adj = torch.Tensor(df["cf_adj"][idx_cf])
+    cur_expl = df["expl_list"][idx_ex][idx_hist]
+
+    sub_adj = torch.Tensor(df["sub_adj"][idx_ex])
+    cf_adj = torch.Tensor(cur_expl[0])
 
     deleted_edges = sub_adj.int() - cf_adj.int()
     deleted_edges[deleted_edges == -1] = 0
@@ -68,11 +73,11 @@ def visualize_generic(df, idx_cf, figsize=(20,15)):
     added_edges = cf_adj.int() - sub_adj.int()
     added_edges[added_edges == -1] = 0
 
-    print("Target node: {}, label: {}".format(df["new_idx"][idx_cf], df["label"][idx_cf]))
-    print("Graph distance loss: {}".format(df["loss_graph_dist"][idx_cf]))
+    print("Target node: {}, label: {}".format(df["new_idx"][idx_ex], df["label"][idx_ex]))
+    print("Graph distance loss: {}".format(cur_expl[2]))
     print("Original prediction: {}, new prediction: {}"
-          .format(df["y_pred_orig"][idx_cf], df["y_pred_new_actual"][idx_cf]))
-    num_nodes = df["num_nodes"][idx_cf]
+          .format(df["y_pred_orig"][idx_ex], cur_expl[1]))
+    num_nodes = df["num_nodes"][idx_ex]
     print("Num of nodes: {}".format(num_nodes))
     num_edges = len(torch.nonzero(sub_adj)) / 2
     print("Num of total edges: {}".format(num_edges))
@@ -89,8 +94,8 @@ def visualize_generic(df, idx_cf, figsize=(20,15)):
 
     # Keep same pos for consistent plotting
     # In case of graph classification the idx of single nodes are not provided
-    if df["new_idx"][idx_cf] is not None:
-        pos = graphviz_layout(adj_graph, root=df["new_idx"][idx_cf])
+    if df["new_idx"][idx_ex] is not None:
+        pos = graphviz_layout(adj_graph, root=df["new_idx"][idx_ex])
     else:
         pos = graphviz_layout(adj_graph)
 
@@ -101,40 +106,40 @@ def visualize_generic(df, idx_cf, figsize=(20,15)):
 
     nx.draw_networkx_nodes(adj_graph, pos, range(num_nodes))
 
-    if df["new_idx"][idx_cf] is not None:
-        nx.draw_networkx_nodes(adj_graph, pos, [df["new_idx"][idx_cf]], node_color="yellow")
+    if df["new_idx"][idx_ex] is not None:
+        nx.draw_networkx_nodes(adj_graph, pos, [df["new_idx"][idx_ex]], node_color="yellow")
 
     nx.draw_networkx_labels(adj_graph, pos)
 
 
-def visualize_by_path(df_path, idx, dataset):
+def visualize_by_path(df_path, idx_ex, idx_hist, dataset):
 
     # Load CF examples
     with open(df_path, "rb") as f:
-        cf_examples = pickle.load(f)
-        num_cf_examples = len(cf_examples)
+        expls = pickle.load(f)
         df_prep = []
 
-        for example in cf_examples:
-            # Ignore examples for which generating a CF wasn't possible
-            if example[0] != []:
-                df_prep.append(example[0])
+        for example in expls:
+            # Ignore examples for which generating an explanation wasn't possible
+            if example[2] != []:
+                df_prep.append(example)
 
-        df = pd.DataFrame(df_prep, columns=header)
+        df = pd.DataFrame(df_prep, columns=header_data)
 
     if dataset == "MUTAG":
-        visualize_mutag(df, idx)
+        visualize_mutag(df, idx_ex, idx_hist)
     else:
-        visualize_generic(df, idx)
+        visualize_generic(df, idx_ex, idx_hist)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', type=str, default=None, help="Path of the file containing the cf")
-    parser.add_argument('--idx', type=int, default=0, help="Id of the cf to visualize")
+    parser.add_argument('--idx_ex', type=int, default=0, help="Id of the explanation to visualize")
+    parser.add_argument('--idx_hist', type=int, default=-1, help="Id of the element in history")
     parser.add_argument('--dataset', type=str, default=None,
                         help="Name of dataset of explanation. Specify for adhoc visualization.")
     args = parser.parse_args()
 
-    visualize_by_path(args.path, args.idx, args.dataset)
+    visualize_by_path(args.path, args.idx_ex, args.idx_hist, args.dataset)
     plt.show()
